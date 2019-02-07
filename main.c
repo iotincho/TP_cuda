275db0a2e9c608a3de240c5b66f5285a70de21ce
Martin <martin.barrera@outlook.com>
2019-02-07 19:47:34 -0300

    con main

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <cuda.h>
 
+#define ARRAY_SIZE 1024;
+#define TILE_WIDTH 64;
+
 #define IS_EVEN(_value_){return _value_ % 2;}
 #define SWAP(_a,_b){int32_t _aux;\
                       _aux = *_a;\
@@ -9,11 +12,6 @@
                       return;}
 
 
-int main(){
-
-
-
-}
 
 __global__
 void odd_even_sort_kernel(int32_t * arr, int32_t n){
@@ -31,4 +29,33 @@ void odd_even_sort_kernel(int32_t * arr, int32_t n){
       }
     }
   }
+}
+
+
+
+int main(void){
+
+int32_t arr[ARRAY_SIZE];
+int32_t *cuda_arr;
+
+dim3 dimGrid (ARRAY_SIZE/TILE_WIDTH +1 ,1 ,1);
+dim3 dimBlock (TILE_WIDTH,1,1);
+
+for (int i = 0; i < ARRAY_SIZE; i++) {
+   arr[i] = rand();
+ }
+
+ cudaMalloc(&cuda_arr,sizeof(int32_t)*ARRAY_SIZE);
+
+ cudaMemcpy(cuda_arr, arr, sizeof(int32_t)*ARRAY_SIZE,cudaMempyHostToDevice);
+
+ odd_even_sort_kernel<<<dimGrid,dimBlock>>>(cuda_arr,ARRAY_SIZE);
+
+ cudaDeviceSynchronize();
+
+ cudaMemcpy(arr, cuda_arr, sizeof(int32_t)*ARRAY_SIZE, cudaMemcpyDeviceToHost);
+
+ cudaFree(cuda_arr);
+
+return 0;
 }
