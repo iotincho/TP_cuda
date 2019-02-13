b7dc4e3af81b66c67d047656f52657eb3664bcad
ser0090 <ser.0090@gmail.com>
2019-02-13 16:50:14 -0300

    cuda a medias

--- main.cu
+++ main.cu
@@ -7,7 +7,7 @@
 #include <device_launch_parameters.h>
 #include <device_functions.h>
 
-#define ARRAY_SIZE 1024
+#define ARRAY_SIZE 12000
 #define TILE_WIDTH 64
 
 //#define IS_EVEN(_value_){return _value_ % 2;}
@@ -18,23 +18,23 @@ inline void SWAP(int32_t *_a,int32_t *_b){int32_t __aux; __aux = *_a; *_a = *_b;
 
 __global__
 void odd_even_sort_kernel(int32_t * arr_d, int32_t n){
-    int32_t tid = blockDim.x * blockIdx.x + threadIdx.x + 1;// +1 corresponde para evitar el overflow en el 0
+    int32_t tid = (blockDim.x * blockIdx.x + threadIdx.x)*2 + 1;// +1 corresponde para evitar el overflow en el 0
 
-    for(int i=0; i<n;i++){
-        if(tid < n) {
-            if (tid%2) { // porque usa tid? deberia usar i
+    for(int32_t i=0; i<n;i++){
+
+            if (i%2 && tid < n-1) { // impar
+                if (arr_d[tid + 1] < arr_d[tid]) {
+                    SWAP(arr_d + tid, arr_d + tid + 1);
+                }
+            }
+            __syncthreads();
+            if(!(i%2) && tid < n){ //par
                 if (arr_d[tid] < arr_d[tid-1]) {
                     SWAP(arr_d + tid, arr_d + tid - 1);
                 }
             }
             __syncthreads();
-            if (!(tid%2)) {
-                if (arr_d[tid] < arr_d[tid-1]) {
-                    SWAP(arr_d + tid, arr_d + tid - 1);
-                }
-            }
-            __syncthreads();
-        }
+
     }
 }
 
@@ -53,10 +53,13 @@ void odd_even_sort_kernel(int32_t * arr_d, int32_t n){
     }
 
 */
-
-int control(int32_t *arr, int32_t *n){
-  for(int i=1;i<n;i++){
-    if(arr[i-1]>arr[i]) return 1;
+__host__
+int control(int32_t *arr, int32_t n){
+  for(int32_t i=1; i<n; i++){
+    if(arr[i-1] > arr[i]){
+        printf("In :: %d\n",i);
+        return 1;
+    }
   }
   return 0;
 }
@@ -65,7 +68,7 @@ int main( int argc, char *argv[] ){
     int32_t arr[ARRAY_SIZE];
     int32_t *cuda_d;
 
-    dim3 dimGrid ((uint)ceil(ARRAY_SIZE / TILE_WIDTH), 1, 1);
+    dim3 dimGrid ((uint)((ARRAY_SIZE / TILE_WIDTH)+1), 1, 1);
     dim3 dimBlock (TILE_WIDTH, 1, 1);
     cudaError_t err;
 
@@ -74,6 +77,9 @@ int main( int argc, char *argv[] ){
       //  printf("%d ", arr[i]);
     }
     printf("\n");
+
+    if(control(arr, ARRAY_SIZE)) printf("desordenado!! \n");
+    else printf("ok!! \n");
 
     err = cudaMalloc(&cuda_d, sizeof(int32_t)*ARRAY_SIZE);
     if( err != cudaSuccess){
@@ -91,7 +97,8 @@ int main( int argc, char *argv[] ){
 
     cudaFree(cuda_d);
 
-    if(control(arr)) printf("desordenado!! \n");
+    if(control(arr, ARRAY_SIZE)) printf("desordenado!! \n");
+    else printf("ok!! \n");
     /*for (int i = 0; i < ARRAY_SIZE; i++) {
         printf("%d ", arr[i]);
     }
