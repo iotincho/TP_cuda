7142ec08e5d60d941f8f8689db641aa66d7977d6
ser0090 <ser.0090@gmail.con>
2019-02-15 12:01:12 -0300

    agregado de marcadores de tiempo

--- main.cu
+++ main.cu
@@ -7,9 +7,12 @@
 #include <device_launch_parameters.h>
 #include <device_functions.h>
 
-#define ARRAY_SIZE 1234577
-#define TILE_WIDTH 1024
+#define MAIN "_MAIN_:"
+#define F1LO "_ODD_:"
+#define CTRL "_CRTL_:"
 
+#define ARRAY_SIZE 53457
+#define TILE_WIDTH 1024
 
 __device__
 inline void SWAP(int32_t *_a,int32_t *_b){int32_t __aux; __aux = *_a; *_a = *_b; *_b = __aux;}
@@ -45,12 +48,16 @@ void odd_even_sort(int32_t * arr, int32_t n){
 	dim3 dimGrid ((uint)((ARRAY_SIZE / TILE_WIDTH)+1), 1, 1);
 	dim3 dimBlock (TILE_WIDTH-1, 1, 1);
 	cudaError_t err;
+	cudaEvent_t start, stop;
+	float mili;
 
 	err = cudaMalloc(&cuda_d, sizeof(int32_t)*ARRAY_SIZE);
-	    if( err != cudaSuccess){
-	        printf("%s in %s at line %d\n", cudaGetErrorString(err), __FILE__, __LINE__); // best definition
-	        exit(EXIT_FAILURE);
-	    }
+	if( err != cudaSuccess){
+		printf("%s in %s at line %d\n", cudaGetErrorString(err), __FILE__, __LINE__); // best definition
+		exit(EXIT_FAILURE);
+	}
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
 
 	cudaMemcpy(cuda_d, arr, sizeof(int32_t)*ARRAY_SIZE, cudaMemcpyHostToDevice);
 
@@ -58,29 +65,29 @@ void odd_even_sort(int32_t * arr, int32_t n){
 	int32_t *p_cuda;
 	int32_t size;
 
-	printf("ordenando..\n");
+	printf("%s ordenando..\n",F1LO);
 	for(int32_t j=0;j<j_limit;j++){
 		p_cuda = cuda_d + (j&1) * TILE_WIDTH;
 		size = n - (j&1) * TILE_WIDTH;
-
+		if(j==0)
+			cudaEventRecord(start);
 		odd_even_sort_kernel<<<dimGrid, dimBlock>>>(p_cuda, size);
 	}
-
-	cudaDeviceSynchronize();
-	printf("terminanding..\n");
+	cudaEventRecord(stop);
+	//cudaDeviceSynchronize();
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&mili, start, stop);
 	cudaMemcpy(arr, cuda_d, sizeof(int32_t)*ARRAY_SIZE, cudaMemcpyDeviceToHost);
 
+	printf("%s terminanding.. time: %f s\n", F1LO, mili/1000);
 	cudaFree(cuda_d);
-
-
-
 }
 
 __host__
 int control(int32_t *arr, int32_t n){
   for(int32_t i=1; i<n; i++){
     if(arr[i-1] > arr[i]){
-        printf("In :: %d\n",i);
+        printf("%s I = %d\n", CTRL, i);
         return 1;
     }
   }
@@ -98,13 +105,13 @@ int main( int argc, char *argv[] ){
     }
     printf("\n");
 
-    if(control(arr, ARRAY_SIZE)) printf("desordenado!! \n");
-    else printf("ok!! \n");
+    if(control(arr, ARRAY_SIZE)) printf("%s desordenado!! \n",MAIN);
+    else printf("%s ok!! \n",MAIN);
 
     odd_even_sort(arr,ARRAY_SIZE);
 
-    if(control(arr, ARRAY_SIZE)) printf("desordenado!! \n");
-    else printf("ok!! \n");
+    if(control(arr, ARRAY_SIZE)) printf("%s desordenado!! \n",MAIN);
+    else printf("%s ok!! \n" ,MAIN);
     /*for (int i = 0; i < ARRAY_SIZE; i++) {
         printf("%d ", arr[i]);
     }
